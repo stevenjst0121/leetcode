4d131a5845b360900e8761d5d351bcc5557b5b43
SITENG JIN <sitengjin@SITENGs-Air-2.fios-router.home>
2020-10-21 21:58:32 -0400

    day 20

---
+++ 227_basic_calculate_2.py
@@ -0,0 +1,145 @@
+from collections import deque
+
+
+class Solution:
+    def calculate(self, s: str) -> int:
+        """Solution 1
+        [MEMO] Brilliant solution using stack
+        """
+        if not s:
+            return 0
+
+        stack = deque()
+        current_num = 0
+        operation = "+"
+        for i, c in enumerate(s):
+            if c.isdigit():
+                current_num = current_num * 10 + int(c)
+
+            if not c.isdigit() and c != " " or i == len(s) - 1:
+                if operation == "-":
+                    stack.append(-1 * current_num)
+                elif operation == "+":
+                    stack.append(current_num)
+                elif operation == "*":
+                    num = stack.pop()
+                    stack.append(num * current_num)
+                elif operation == "/":
+                    num = stack.pop()
+                    stack.append(int(num / current_num))
+
+                operation = c
+                current_num = 0
+
+        result = 0
+        while stack:
+            result += stack.pop()
+        return result
+
+    def calculate(self, s: str) -> int:
+        """Solution 2
+        [MEMO] Brilliant solution without using stack
+        """
+        if not s:
+            return 0
+
+        result = 0
+        last_num = 0
+        current_num = 0
+        operation = "+"
+        for i, c in enumerate(s):
+            if c.isdigit():
+                current_num = current_num * 10 + int(c)
+
+            if not c.isdigit() and c != " " or i == len(s) - 1:
+                if operation == "+" or operation == "-":
+                    result += last_num
+                    last_num = current_num if operation == "+" else (-1 * current_num)
+                elif operation == "+":
+                    last_num += current_num
+                elif operation == "*":
+                    last_num *= current_num
+                elif operation == "/":
+                    last_num = int(last_num / current_num)
+
+                operation = c
+                current_num = 0
+
+        result += last_num
+        return result
+
+    # def calculate(self, s: str) -> int:
+    #     """Draft 1
+    #     Idea is to define one function that calculates s that only contains * and /
+    #     And another function that utlizes the above function to calculate entire string
+    #     """
+    #     s = s.replace(" ", "")
+    #     return self.calculatePM(s)
+
+    # def calculatePM(self, s: str) -> int:
+    #     result = None
+    #     mid = 0
+    #     for i, c in enumerate(s):
+    #         if c == "+" or c == "-":
+    #             if mid == 0:
+    #                 result = self.calculateMD(s[:i])
+    #             else:
+    #                 second = self.calculateMD(s[mid + 1 : i])
+    #                 if s[mid] == "+":
+    #                     result += second
+    #                 elif s[mid] == "-":
+    #                     result -= second
+    #                 else:
+    #                     print(f"ERROR - {s}")
+    #                     exit(1)
+    #             mid = i
+
+    #     if mid > 0:
+    #         second = self.calculateMD(s[mid + 1 :])
+    #         if s[mid] == "+":
+    #             result += second
+    #         elif s[mid] == "-":
+    #             result -= second
+    #         else:
+    #             print(f"ERROR - {s}")
+    #             exit(1)
+    #     else:
+    #         result = self.calculateMD(s)
+
+    #     return result
+
+    # def calculateMD(self, s: str) -> int:
+    #     # s should only contain * or / operators or none
+    #     if "*" not in s and "/" not in s:
+    #         return int(s)
+
+    #     result = None
+    #     mid = 0
+    #     for i, c in enumerate(s):
+    #         if c == "*" or c == "/":
+    #             if mid == 0:
+    #                 result = int(s[:i])
+    #             else:
+    #                 second = int(s[mid + 1 : i])
+    #                 if s[mid] == "*":
+    #                     result *= second
+    #                 elif s[mid] == "/":
+    #                     result //= second
+    #                 else:
+    #                     print(f"ERROR - {s}")
+    #                     exit(1)
+    #             mid = i
+
+    #     if mid > 0:
+    #         second = int(s[mid + 1 :])
+    #         if s[mid] == "*":
+    #             result *= second
+    #         elif s[mid] == "/":
+    #             result //= second
+    #         else:
+    #             print(f"ERROR - {s}")
+    #             exit(1)
+    #     else:
+    #         result = int(s)
+
+    #     return result
