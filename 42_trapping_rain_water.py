f940b191a346984fd14c5b13803efa739e6c7d9d
SITENG JIN <sitengjin@SITENGs-Air-2.fios-router.home>
2021-03-15 23:01:03 -0400

    fb review day 64 till 953

--- 42_trapping_rain_water.py
+++ 42_trapping_rain_water.py
@@ -4,29 +4,8 @@ from collections import deque
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        """Stack solution
-        [MEMO] It's gonna be hard to think of it...
-        """
-        ans = 0
-        curr = 0
-        size = len(height)
-        stack = deque()
-        while curr < size:
-            while stack and height[curr] > height[stack[0]]:
-                top = stack.popleft()
-                if not stack:
-                    break
-                distance = curr - stack[0] - 1
-                bounded_height = min(height[curr], height[stack[0]]) - height[top]
-                ans += distance * bounded_height
-            stack.appendleft(curr)
-            curr += 1
-
-        return ans
-
-    def trap(self, height: List[int]) -> int:
         """Solution DP
-        [MEMO] brilliant solution using DP by storing max from both sides
+        [MEMO+1] brilliant solution using DP by storing max from both sides
         """
         if not height:
             return 0
@@ -47,6 +26,55 @@ class Solution:
 
         for i in range(1, size - 1):
             ans += min(left_max[i], right_max[i]) - height[i]
+        return ans
+
+    def trap(self, height: List[int]) -> int:
+        """Solution Two Pointer
+        [MEMO+1] Use the concept of Solution DP that only once side max matters
+        Same O(n), but just reduces the number of iterations
+        """
+        if not height:
+            return 0
+
+        ans = 0
+        left = 0
+        right = len(height) - 1
+        left_max, right_max = 0, 0
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] >= left_max:
+                    left_max = height[left]
+                else:
+                    ans += left_max - height[left]
+                left += 1
+            else:
+                if height[right] >= right_max:
+                    right_max = height[right]
+                else:
+                    ans += right_max - height[right]
+                right -= 1
+        return ans
+
+    def trap(self, height: List[int]) -> int:
+        """Stack solution
+        It's gonna be hard to think of it...
+        Similar to Solution DP but only loop through once
+        """
+        ans = 0
+        curr = 0
+        size = len(height)
+        stack = deque()
+        while curr < size:
+            while stack and height[curr] > height[stack[0]]:
+                top = stack.popleft()
+                if not stack:
+                    break
+                distance = curr - stack[0] - 1
+                bounded_height = min(height[curr], height[stack[0]]) - height[top]
+                ans += distance * bounded_height
+            stack.appendleft(curr)
+            curr += 1
+
         return ans
 
     def trap(self, height: List[int]) -> int:
--- 953_verifying_alien_dict.py
+++ 953_verifying_alien_dict.py
@@ -1,28 +1,48 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        if not words:
-            return True
-
-        # init ordermap
         ordermap = {}
         for i, c in enumerate(order):
             ordermap[c] = i
 
-        for i in range(len(words) - 1):
-            word_1 = words[i]
-            word_2 = words[i + 1]
-            min_len = min(len(word_1), len(word_2))
-            is_same = True  # flag if all letters within min_len are same
-            for j in range(min_len):
-                if ordermap[word_1[j]] < ordermap[word_2[j]]:
-                    is_same = False
+        for word_1, word_2 in zip(words[:-1], words[1:]):
+            size = min(len(word_1), len(word_2))
+            for i in range(size):
+                if word_1[i] == word_2[i]:
+                    continue
+                elif ordermap[word_1[i]] < ordermap[word_2[i]]:
+                    # good case
                     break
-                elif ordermap[word_1[j]] > ordermap[word_2[j]]:
+                else:
                     return False
-                # If letters are same, do nothing
-
-            # is_same check
-            if is_same and len(word_1) > len(word_2):
-                return False
-
+            else:
+                if len(word_1) > len(word_2):
+                    return False
         return True
+
+    # def isAlienSorted(self, words: List[str], order: str) -> bool:
+    #     if not words:
+    #         return True
+
+    #     # init ordermap
+    #     ordermap = {}
+    #     for i, c in enumerate(order):
+    #         ordermap[c] = i
+
+    #     for i in range(len(words) - 1):
+    #         word_1 = words[i]
+    #         word_2 = words[i + 1]
+    #         min_len = min(len(word_1), len(word_2))
+    #         is_same = True  # flag if all letters within min_len are same
+    #         for j in range(min_len):
+    #             if ordermap[word_1[j]] < ordermap[word_2[j]]:
+    #                 is_same = False
+    #                 break
+    #             elif ordermap[word_1[j]] > ordermap[word_2[j]]:
+    #                 return False
+    #             # If letters are same, do nothing
+
+    #         # is_same check
+    #         if is_same and len(word_1) > len(word_2):
+    #             return False
+
+    #     return True
